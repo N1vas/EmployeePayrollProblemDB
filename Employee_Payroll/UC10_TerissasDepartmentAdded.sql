7015d50796a0d9d166882eba7fc03922b204eb39
N1vas <ggj.nivas.18@gmail.com>
2020-11-02 21:49:38 +0530

    [Nivas] Add : Added SQL files from UC 8 to 10

---
+++ Employee_Payroll/UC10_TerissasDepartmentAdded.sql
@@ -0,0 +1,2 @@
+insert into employee_payroll values
+('Terisa', 30000.00,'2020-05-21','F','America','9878765689','Marketing and Sales',5000,3000,2500,22000)
---
+++ Employee_Payroll/UC9_ExtendTablewithMoreFields.sql
@@ -0,0 +1,5 @@
+Alter table employee_payroll add
+Deduction float,
+Taxable_pay real,
+Incometax float,
+NetPay real;
